7777245e928f92b044117b34d02659589f3f0428
Italo de Pontes Oliveira <italodepontesoliveira93@gmail.com>
2024-08-11 20:14:07 -0300

    finish project.

--- streamlit/pages/Licitacoes.py
+++ streamlit/pages/Licitacoes.py
@@ -20,7 +20,8 @@ def convert_float(value):
         return value
 
 for column in ['valor_proposta', 'valor_homologacao',
-               'valor_empenhado', 'valor_pago']:
+               'valor_empenhado', 'valor_pago',
+               'valor_pago_total']:
     df[column] = df[column].apply(lambda x: convert_float(x))
 
 top_k = 10
@@ -41,3 +42,41 @@ for column in ['numero_licitacao', 'ente', 'unidade_gestora']:
     st.text(map_text[column])
     st.dataframe(df_top)
 
+
+
+
+st.title('Top Discrepância: Pago - Proposta (valor absoluto)')
+valor1 = 'valor_pago_total'
+valor2 = 'valor_proposta'
+df['Discrepancia'] = df[valor1] - df[valor2]
+df_top = df.nlargest(10, 'Discrepancia')
+df_top = pd.melt(df_top, id_vars=['unidade_gestora'],
+                         value_vars=[valor1,
+                                     valor2])
+df_top.columns = ['Unidade_Gestora', 'Tipo Pagamento', 'Valor']
+st.bar_chart(df_top, x="Unidade_Gestora",
+                     y="Valor",
+                     color="Tipo Pagamento", stack=False)
+
+
+st.title('Top Discrepância: Pago - Proposta (valor relativo)')
+df['Discrepancia Relativa'] = df['Discrepancia']/df[valor1]
+df_top_r = df.nlargest(10, 'Discrepancia Relativa')
+df_top_r = pd.melt(df_top_r, id_vars=['unidade_gestora'],
+                         value_vars=[valor1,
+                                     valor2])
+df_top_r.columns = ['Unidade_Gestora', 'Tipo Pagamento', 'Valor']
+st.bar_chart(df_top_r, x="Unidade_Gestora",
+                     y="Valor",
+                     color="Tipo Pagamento", stack=False)
+
+
+st.title('Top Discrepância: Busca por Ente')
+feat = 'ente'
+data = 'data_homologacao'
+valor = 'valor_homologacao'
+text = st.text_input("Digite os Ente a serem analisados:.")
+text = [word.lower() for word in text.split(',')]   
+df_agg = df[df[feat].apply(lambda x: True if x.lower() in text else False)]
+df_agg = df_agg.groupby([data, feat])[valor].sum().reset_index()
+st.line_chart(df_agg, x=data, y=valor, color=feat)
